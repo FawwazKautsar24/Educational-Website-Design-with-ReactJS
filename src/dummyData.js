1e47396c33c6967f28aaa5fc87bf4e82f2f72311
FawwazKats <fawwazkautsar1907912@upi.edu>
2022-12-15 00:05:38 +0700

    Membuat component Price, PriceCard & men-styling Price, PriceCard

--- src/dummyData.js
+++ src/dummyData.js
@@ -313,3 +313,26 @@ export const testimonal = [
     cover: "./images/testo/t3.webp",
   },
 ];
+
+export const price = [
+  {
+    name: "BASIC PLAN",
+    price: "49K",
+    desc: "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.",
+  },
+  {
+    name: "BEGINNER PLAN",
+    price: "79K",
+    desc: "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.",
+  },
+  {
+    name: "PREMIUM PLAN",
+    price: "109k",
+    desc: "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.",
+  },
+  {
+    name: "ULTIMATE PLAN",
+    price: "149K",
+    desc: "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.",
+  },
+];
