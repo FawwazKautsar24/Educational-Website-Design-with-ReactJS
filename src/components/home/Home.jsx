be49dedfe0ed26f2ff48fd7586cf4140e8cff2a0
FawwazKats <fawwazkautsar1907912@upi.edu>
2022-12-18 09:46:59 +0700

    Membuat component HBlog, Hprice

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -3,6 +3,8 @@ import AboutCard from '../about/AboutCard';
 import HAbout from './HAbout';
 import Hero from './hero/Hero';
 import Test from './testimonial/Test';
+import HBlog from './HBlog';
+import HPrice from './HPrice';
 
 const Home = () => {
     return (
@@ -11,6 +13,8 @@ const Home = () => {
             <AboutCard />
             <HAbout />
             <Test />
+            <HBlog />
+            <HPrice />
         </>
     );
 }
